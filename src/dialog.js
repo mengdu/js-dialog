a213ca03f0b6d36c47e35ed3fd1b84851f70cd31
mengdu <1752295326@qq.com>
2019-03-21 23:46:16 +0800

    feat: 添加 渐入动画

--- src/dialog.js
+++ src/dialog.js
@@ -12,26 +12,44 @@ function createElement (tag, attrs = {}) {
 
 export default class Dialog {
   constructor (options) {
-    this.WrapperEl = createElement('DIV', {
-      className: 'js-dialog-wrapper'
-    })
-    this.MaskEL = createElement('DIV', {
-      className: 'js-dialog-mask'
-    })
-    this.DialogEl = createElement('DIV', {
-      className: 'js-dialog'
-    })
-    this.DialogContentEl = createElement('DIV', {
-      className: 'js-dialog-content'
-    })
+    this.dom = {
+      warpper: createElement('DIV', {
+        className: 'js-dialog-wrapper'
+      }),
+      mask: createElement('DIV', {
+        className: 'js-dialog-mask'
+      }),
+      dialog: createElement('DIV', {
+        className: 'js-dialog'
+      }),
+      content: createElement('DIV', {
+        className: 'js-dialog-content'
+      })
+    }
+    const that = this
+    this.visibile = true
 
-    this.DialogEl.appendChild(this.DialogContentEl)
-    this.WrapperEl.appendChild(this.MaskEL)
-    this.WrapperEl.appendChild(this.DialogEl)
+    this.dom.dialog.appendChild(this.dom.content)
+    this.dom.warpper.appendChild(this.dom.mask)
+    this.dom.warpper.appendChild(this.dom.dialog)
+
+    this.dom.dialog.addEventListener('animationend', function () {
+      console.log(arguments, that.visibile)
+      if (!that.visibile) {
+        that.dom.warpper.style.display = 'none'
+      }
+    }, false)
   }
 
   show (content) {
-    this.DialogContentEl.innerHTML = content
-    document.body.appendChild(this.WrapperEl)
+    this.dom.content.innerHTML = content
+    this.visibile = true
+    this.dom.warpper.style.display = ''
+    document.body.appendChild(this.dom.warpper)
+  }
+
+  hide () {
+    this.visibile = false
+    this.dom.dialog.classList.add('js-dialog-leave')
   }
 }
