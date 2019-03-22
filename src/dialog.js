0df5110bca8b1eee61962bc5b5d67f58944898d9
v_yguiliu <v_yguiliu@tencent.com>
2019-03-22 10:28:23 +0800

    feat: 实现 show/hide 过渡动画

--- src/dialog.js
+++ src/dialog.js
@@ -10,8 +10,15 @@ function createElement (tag, attrs = {}) {
   return el
 }
 
+function on (el, event, handle) {
+  el.addEventListener(event, handle, false)
+}
+
 export default class Dialog {
   constructor (options) {
+    const that = this
+    this.options = options = options || {}
+
     this.dom = {
       warpper: createElement('DIV', {
         className: 'js-dialog-wrapper'
@@ -22,34 +29,61 @@ export default class Dialog {
       dialog: createElement('DIV', {
         className: 'js-dialog'
       }),
+      header: createElement('DIV', {
+        className: 'js-dialog-header'
+      }),
       content: createElement('DIV', {
         className: 'js-dialog-content'
       })
     }
-    const that = this
-    this.visibile = true
 
+    this.visibile = false
+
+    this.dom.dialog.appendChild(this.dom.header)
     this.dom.dialog.appendChild(this.dom.content)
     this.dom.warpper.appendChild(this.dom.mask)
     this.dom.warpper.appendChild(this.dom.dialog)
 
-    this.dom.dialog.addEventListener('animationend', function () {
-      console.log(arguments, that.visibile)
+    this.dom.header.innerHTML = '<span>' + (options.title || '提示') + '</span><button class="js-dialog--close"><span>×</span></button>'
+    on(this.dom.dialog, 'animationend', function () {
+      console.log('animationend', that.visibile)
       if (!that.visibile) {
         that.dom.warpper.style.display = 'none'
+
+        setTimeout(function () {
+          that.dom.mask.classList.remove('js-dialog-mask-leave')
+          that.dom.dialog.classList.remove('js-dialog-enter')
+          that.dom.dialog.classList.remove('js-dialog-leave')
+        })
       }
-    }, false)
+    })
+
+    on(this.dom.mask, 'click', function () {
+      that.hide()
+    })
+
+    this.updateContent(options.content)
+    this.dom.warpper.style.display = 'none'
+    document.body.appendChild(this.dom.warpper)
   }
 
-  show (content) {
-    this.dom.content.innerHTML = content
-    this.visibile = true
+  show () {
+    this.dom.dialog.classList.add('js-dialog-enter')
     this.dom.warpper.style.display = ''
-    document.body.appendChild(this.dom.warpper)
+    this.visibile = true
   }
 
   hide () {
     this.visibile = false
     this.dom.dialog.classList.add('js-dialog-leave')
+    this.dom.mask.classList.add('js-dialog-mask-leave')
+  }
+
+  updateContent (content) {
+    if (this.options.supportHTML) {
+      this.dom.content.innerHTML = content
+    } else {
+      this.dom.content.innerText = content
+    }
   }
 }
