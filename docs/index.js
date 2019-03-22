f598fc7b60f1841de8a4fae05f6a5d588896a471
v_yguiliu <v_yguiliu@tencent.com>
2019-03-22 20:05:17 +0800

    docs: 更新例子

--- docs/index.js
+++ docs/index.js
@@ -2,9 +2,17 @@
 import Dialog from '../src/index'
 
 window.addEventListener('load', function () {
-  const dialog = new Dialog({})
+  const dialog = new Dialog({
+    content: '<h2>Hello world !</h2><p>This is a test.</p>',
+    supportHTML: true
+  })
 
-  dialog.show('Hello world !')
+  // dialog.show()
+
+  document.getElementById('btn').addEventListener('click', function () {
+    dialog.show()
+  })
+
   window.dialog = dialog
 })
 
