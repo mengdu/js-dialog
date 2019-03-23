c50087609c6569f2f55e1f129cd2767dc65fdeb8
mengdu <1752295326@qq.com>
2019-03-23 22:18:49 +0800

    docs: 更新例子

--- docs/index.js
+++ docs/index.js
@@ -3,14 +3,59 @@ import Dialog from '../src/index'
 
 window.addEventListener('load', function () {
   const dialog = new Dialog({
-    content: '<h2>Hello world !</h2><p>This is a test.</p>',
-    supportHTML: true
+    content: '<h2>Hello world !</h2><p>这是提示信息。</p>',
+    // hasHeader: false,
+    // hasCloseButton: false,
+    supportHTML: true,
+    cancelButtonText: '取消',
+    confirmButtonText: '确定',
+    onClose () {
+      console.log('close')
+    },
+    onOpen () {
+      console.log('open')
+    },
+    cancel () {
+      console.log('cancel')
+      dialog.hide()
+    },
+    confirm () {
+      console.log('confirm')
+      dialog.hide()
+    }
   })
 
-  // dialog.show()
+  console.log(dialog)
 
   document.getElementById('btn').addEventListener('click', function () {
-    dialog.show()
+    if (dialog.visible) {
+      dialog.hide()
+    } else {
+      dialog.show()
+    }
+  })
+
+  const dialog2 = new Dialog({
+    el: document.getElementById('box1'),
+    content: 'Hello !',
+    cancel () {
+      console.log('cancel')
+      dialog2.hide()
+    },
+    confirm () {
+      console.log('confirm')
+      dialog2.hide()
+    }
+  })
+
+  dialog2.show()
+
+  document.getElementById('btn2').addEventListener('click', function () {
+    if (dialog2.visible) {
+      dialog2.hide()
+    } else {
+      dialog2.show()
+    }
   })
 
   window.dialog = dialog
